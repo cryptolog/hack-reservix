c0714525e16a242a1bad79b46921907164b99030
Florian Postel <florian.postel@btc-ag.com>
2015-05-29 13:37:57 +0200

    aufgeräumt

--- src/main/java/de/btcag/hackathon/Boundary/ReservationRest.java
+++ src/main/java/de/btcag/hackathon/Boundary/ReservationRest.java
@@ -2,6 +2,8 @@ package de.btcag.hackathon.boundary;
 
 import de.btcag.hackathon.model.dao.TerminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import de.btcag.hackathon.business.ReservationService;
@@ -25,6 +27,25 @@ public class ReservationRest {
     @RequestMapping( method = RequestMethod.GET)
     public Termin find(@RequestParam()Integer id) {
         return terminRepository.findOne(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<Termin> update(@RequestBody Termin termin){
+        if(terminRepository.findOne(termin.getId())!=null) {
+            return new ResponseEntity<Termin>(terminRepository.save(termin), HttpStatus.OK);
+        }{
+            return new ResponseEntity<Termin>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity delete(@RequestBody Termin termin){
+        if(terminRepository.findOne(termin.getId())!= null) {
+            terminRepository.delete(termin);
+        }else{
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
